20b367062e668c1851a490dd9decda2976876c41
OrtizNicola <130187770+OrtizNicola@users.noreply.github.com>
2024-10-03 00:32:16 -0500

    add feature to resize the grid

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const GRID_SIZE = 16;
 const CONTAINER_SIZE = 500;
-const PIXEL_SIZE = CONTAINER_SIZE / GRID_SIZE;
 
-let container = document.querySelector(".container")
+let container = document.querySelector(".container");
+let resizeBtn = document.querySelector(".resize");
+let input = document.querySelector("#size");
 
-for(let i = 0; i < Math.pow(GRID_SIZE, 2); i++) {
-    let pixel = document.createElement("div");
-    pixel.style.cssText = `width: ${PIXEL_SIZE}px; 
-                           height: ${PIXEL_SIZE}px; 
-                           border: solid black 1px; 
-                           box-sizing: border-box;`;
+function createGrid(gridSize) {
+    container.textContent = "";
+    let pixelSize = CONTAINER_SIZE / gridSize;
+    for(let i = 0; i < Math.pow(gridSize, 2); i++) {
+        let pixel = document.createElement("div");
+        pixel.style.cssText = `width: ${pixelSize}px; 
+                               height: ${pixelSize}px; 
+                               border: solid black 0.5px; 
+                               box-sizing: border-box;`;
 
-    pixel.addEventListener("mouseenter", () => {
-        pixel.style.backgroundColor = "pink";
-    })
+        pixel.addEventListener("mouseenter", () => {
+            pixel.style.backgroundColor = "purple";
+        })
 
-    container.appendChild(pixel);
-
-    pixel
+        container.appendChild(pixel);
+    }
 }
+
+resizeBtn.addEventListener("click", () => {
+    createGrid(input.value);
+})
+
+createGrid(GRID_SIZE);
